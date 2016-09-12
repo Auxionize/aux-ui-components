e650438b0b9369fb060dc32c6b629a3bcc4d8b7a
Branimir Simeonov <br.simeonov@gmail.com>
2016-09-12 16:30:46 +0300

    Added aux-label-state

--- dist/aux-ui-components.js
+++ dist/aux-ui-components.js
@@ -27,3 +27,24 @@ angular.module('aux-ui-components').component('offerType', {
 			'<i ng-if="::$ctrl.value===\'secret\'"      class="fa fa-circle"    translated-title="Secret"></i>' +
 		'</span>'
 });
+
+angular.module('aux-ui-components').component('auxLabelState', {
+	bindings: {
+		state: '='
+	},
+	template: function ($element, $attrs) {
+		var icon = '';
+		switch ($attrs.state) {
+			case 'draft':       icon = 'fa-file';           break;
+			case 'active':      icon = 'fa-cogs';           break;
+			case 'won':         icon = 'fa-trophy';         break;
+			case 'finished':    icon = 'fa-flag-checkered'; break;
+			case 'rejected':    icon = 'fa-ban';            break;
+			default: icon = 'fa-file'; break;
+		}
+		return  '<span class="label label-default text-upper state ' + $attrs.state + '">' +
+					'<i class="fa ' + icon + '">&nbsp;</i>' +
+					'<span translate translate-context="purchase-state">' + $attrs.state + '</span>' +
+				'</span>';
+	},
+});
